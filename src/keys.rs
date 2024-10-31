34c18346c84635f66ca33c83a4ddc88210c6e30f
Aleksandr Belousov <aleksandr.b@simple.app>
2024-10-31 22:38:53 +0300

    fixed

--- src/keys.rs
+++ src/keys.rs
@@ -4,8 +4,15 @@ pub const GOOGLE_ANALYTICS_KEY: &str = "google_analytics";
 
 pub const CLICK_ID_KEY: &str = "click_id";
 
+pub const AFFILIATE_ID_KEY: &str = "affiliate_id";
+
 pub fn get_click_id_key(affiliate_integration_settings: AffiliateIntegrationSettings) -> String {
     let key = format!("{}_{}", CLICK_ID_KEY, affiliate_integration_settings.to_string());
+    key
+}
+
+pub fn get_affiliate_id_key(affiliate_integration_settings: AffiliateIntegrationSettings) -> String {
+    let key = format!("{}_{}", AFFILIATE_ID_KEY, affiliate_integration_settings.to_string());
     key
 }
 
@@ -23,5 +30,17 @@ mod tests {
     fn test_get_click_id_key_keitaro() {
         let key = get_click_id_key(AffiliateIntegrationSettings::Keitaro);
         assert_eq!(key, "click_id_keitaro");
+    }
+
+    #[test]
+    fn test_get_affiliate_id_key_acqufy() {
+        let key = get_affiliate_id_key(AffiliateIntegrationSettings::Acqufy);
+        assert_eq!(key, "affiliate_id_acqufy");
+    }
+
+    #[test]
+    fn test_get_affiliate_id_key_keitaro() {
+        let key = get_affiliate_id_key(AffiliateIntegrationSettings::Keitaro);
+        assert_eq!(key, "affiliate_id_keitaro");
     }
 }
