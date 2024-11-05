21a19943408838c8e5f658d45deac6f126f831f9
Aleksandr Belousov <aleksandr.b@simple.app>
2024-11-05 12:27:28 +0300

    removed deprecated

--- src/google_analytics.rs
+++
@@ -1,9 +0,0 @@
-use serde::*;
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
-pub struct GoogleAnalyticsKeyValue {
-    pub client_id_ga: String,
-    pub session_id_ga: String,
-    pub gclid: String,
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 
 pub mod keys;
-pub mod google_analytics;
-
 pub use keys::*;
-pub use google_analytics::*;
+
 
 pub mod affiliate_integration_settings;
 pub use affiliate_integration_settings::*;
